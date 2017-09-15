45d8469c0b1b7c8f42cb338b85a4f73d2af21d3d
Gideon <ogjr80@gmail.com>
2017-09-15 10:03:49 +0200

    added bubble charts

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
-import {Bar} from 'react-chartjs-2';
+import {Bar, Bubble} from 'react-chartjs-2';
 
 class Chart extends Component{
   constructor(props){
     super(props);
     this.state = {
+      bubbleData: {
+        labels: ['January'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [{x:10,y:20,r:5}, {x:85,y:7,r:7}]
+    }
+  ]
+},
       chartData : {
         labels: ['Johannesburg', 'Durban', 'CapeTown','Port Elizabet', 'Western Cape', 'Easter Cape'],
         datasets:[
@@ -39,9 +65,38 @@ class Chart extends Component{
         <div className="app">
           <Bar
             data={this.state.chartData}
-            height ={20}
-            width = {50}
-            option={{maintainAspectRatio: false}}
+            //height ={150}
+            //width = {30}
+            options = {{
+              titles : {
+              display: true,
+              text: 'Largest Cities in South Africa',
+              fontSize: 25
+            },
+            legend:{
+             display: true,
+             position: 'right',
+             labels: {
+               fontColor: '#00'
+              }
+            },
+            layout: {
+             padding: {
+              left: 50,
+              right: 0,
+              bottom: 0,
+              top: 0
+             }
+            },
+            tooltips: {
+             enabled: true
+            }
+            }
+            }
+          />
+
+          <Bubble
+            data={this.state.bubbleData}
           />
         </div>
     )
